b16b5544e14fb899489064a3032d2ce7a412265c
Usuario <Usuario@CPD02-M18>
2022-10-05 15:57:17 -0300

    Adicionar arquivos de projeto.

---
+++ AppDSEtec/Model/MenuItens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDSEtec.Model
+{
+    public class MenuItens
+    {
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
