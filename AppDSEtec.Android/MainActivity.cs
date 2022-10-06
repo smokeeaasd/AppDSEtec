c1c6c7e3e33997d6d89bec980747ca4ccee325b1
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2022-10-06 15:59:40 -0300

    terceiro commit

--- AppDSEtec.Android/MainActivity.cs
+++ AppDSEtec.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace AppDSEtec.Droid
 {
-    [Activity(Label = "AppDSEtec", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "AppDSEtec", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
