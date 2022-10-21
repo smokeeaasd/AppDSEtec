813fb3c2b5284925ddd901fbb9895d3ba8b5375d
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2022-10-21 15:55:15 -0300

    terceiro commit

--- AppDSEtec/AssemblyInfo.cs
+++ AppDSEtec/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
+[assembly: ExportFont("EpilogueRegular.ttf", Alias = "EpilogueRegular")]
+[assembly: ExportFont("UbuntuMedium.ttf", Alias = "UbuntuMedium")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
